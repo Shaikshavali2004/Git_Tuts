cd6ac71204069ded7d531829f11da11abc8f3f4c
Shaikshavali2004 <shaikshavali1019@gmail.com>
2025-05-21 20:57:20 +0530

    Data Types in python

---
+++ Datatypes.py
@@ -0,0 +1,34 @@
+eid=101
+esal=45000.45
+ename='Rahul'
+c = 10+20j
+avail=False 
+uids=[101,102,103,101,103]
+enames=('RG','SG','PG','NM')
+eids={101,102,103,101,102}
+emp={
+    'eid':101,
+    'ename':'Rahul'
+}
+b=bytes([10,20,30,40])
+ba=bytearray([10,20,30,40])
+fz=frozenset({101,102,103})
+r=range(100)
+nt=None 
+
+
+
+print(type(eid))
+print(type(esal))
+print(type(ename))
+print(type(c))
+print(type(avail))
+print(type(uids))
+print(type(enames))
+print(type(eids))
+print(type(emp))
+print(type(b)) 
+print(type(ba)) 
+print(type(fz))
+print(type(r))  
+print(type(nt))
